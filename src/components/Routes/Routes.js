f6fc653d34583476500839f091a307d97a24b1a1
ChihYang41 <Jigsaw0401@gmail.com>
2020-03-22 18:06:06 +0800

    change components to lazy components

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import React, { Suspense } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
-import Movies from '../../containers/Movies';
-import Movie from '../../containers/Movie';
-import SearchResults from '../../containers/SearchResults';
-import RegisterForm from '../../containers/Register';
-import LoginForm from '../../containers/Login';
-import WatchList from '../../containers/WatchList';
+import Loading from '../loading/Loading'
+
+const Movies = React.lazy(() => import('../../containers/Movies'))
+const Movie = React.lazy(() => import('../../containers/Movie'))
+const SearchResults = React.lazy(() => import('../../containers/SearchResults'))
+const RegisterForm = React.lazy(() => import('../../containers/Register'))
+const LoginForm = React.lazy(() => import('../../containers/Login'))
+const WatchList = React.lazy(() => import('../../containers/WatchList'))
 
 export default function Routes() {
   return (
-    <Switch>
-      <Route exact path="/register" component={RegisterForm} />
-      <Route exact path="/login" component={LoginForm} />
-      <Route exact path="/watchlist" component={WatchList} />
-      <Route exact path="/movie/:movie_id"  component={Movie} />
-      <Route exact path="/search/:query" component={SearchResults} />
-      <Route exact path="/:filter?/:page" component={Movies} />
-      <Redirect to="/popular/1"/>
-    </Switch>
+    <Suspense fallback={<Loading/>}>
+      <Switch>
+        <Route exact path="/register" component={RegisterForm} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/watchlist" component={WatchList} />
+        <Route exact path="/movie/:movie_id" component={Movie} />
+        <Route exact path="/search/:query" component={SearchResults} />
+        <Route exact path="/:filter?/:page" component={Movies} />
+        <Redirect to="/popular/1" />
+      </Switch>
+    </Suspense>
   )
 }
